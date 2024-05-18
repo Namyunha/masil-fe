6baa412dc94ccba4067ff09f60129b50a9d4afc8
aaaz425 <ytokogg@gmail.com>
2024-05-19 01:42:14 +0900

    feat: msw, react query, use react form 활용 예제 구현

--- src/components/SignUp/ButtonWrapper.tsx
+++ src/components/SignUp/ButtonWrapper.tsx
@@ -1,27 +1,37 @@
 'use client';
 
+import { useForm } from 'react-hook-form';
+import { useSignUpQuery } from '@/api/sign/queries';
 import useResponsive from '@/hooks/useResponsive';
 import Button from '../Button';
 
 export default function ButtonWrapper() {
   const isMobile = useResponsive();
 
+  const { register, watch, handleSubmit } = useForm();
+
+  const { mutate: onRegister } = useSignUpQuery({
+    email: watch('email'),
+    nickName: watch('nickName'),
+    password: watch('password'),
+  });
+
   return (
     <>
-      <Button
-        value="회원가입"
-        onClick={() => {
-          console.log('회원가입');
-        }}
-      />
-      {isMobile && (
-        <Button
-          value="취소"
-          onClick={() => {
-            console.log('취소');
-          }}
-        />
-      )}
+      <form onSubmit={handleSubmit(() => onRegister())}>
+        <input {...register('email')} />
+        <input {...register('nickName')} />
+        <input {...register('password')} />
+        <Button type="submit">회원가입</Button>
+        {isMobile && (
+          <Button
+            value="취소"
+            onClick={() => {
+              console.log('취소');
+            }}
+          />
+        )}
+      </form>
     </>
   );
 }
