68dd1db6245c0eafa87e6ce6c0484c07300efb51
aaaz425 <ytokogg@gmail.com>
2024-05-28 05:16:02 +0900

    chore: axios 패키지 설치 및 fetcher 함수 변경

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -1,42 +1,49 @@
+import axios, { AxiosError } from 'axios';
 import { API_BASE_URL } from '@/constants/api';
 
-type FetcherType = {
-  endPoint: string;
-  method?: string;
-  headers?: HeadersInit;
-  body?: BodyInit;
-};
+export const fetcher = axios.create({
+  baseURL: API_BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+});
 
-export async function fetcher({
-  endPoint,
-  method,
-  headers,
-  body,
-}: FetcherType) {
-  // Todo: 백엔드와 API 문서 작업 이후 default 옵션 추가할 것
-  if (!(body instanceof FormData)) {
-    headers = {
-      'Content-Type': 'application/json',
-      ...headers,
-    };
-  }
-
-  const options = {
-    method,
-    headers,
-    body,
-  };
-
-  try {
-    const response = await fetch(`${API_BASE_URL}${endPoint}`, options);
-
-    if (!response.ok) {
-      return Promise.reject(response);
+fetcher.interceptors.request.use(
+  (config) => {
+    // Memo: 토큰 저장 위치에 따라 로직 변경해야함
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
     }
+    return config;
+  },
+  (error) => {
+    // Memo: 요청 자체가 실패했을 경우
+    Promise.reject(error);
+  }
+);
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
+fetcher.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    if (error.response) {
+      // Memo: 에러 응답이 있는 경우
+      const { data } = error.response as {
+        status: number;
+        data: { status: number; message: string };
+      };
+
+      // Memo: 커스텀 에러 메세지가 없는 경우
+      if (!data.message) {
+        throw new Error(error.message);
+      }
+
+      throw new Error(data.message);
+    } else if (error.request) {
+      // Memo: 요청이 완료되었으나 응답이 없는 경우
+      console.log(error.request);
+    } else {
+      // Memo: 요청을 보내기 전에 에러가 발생한 경우
+      console.log('Error', error.message);
+    }
     return Promise.reject(error);
   }
-}
+);
