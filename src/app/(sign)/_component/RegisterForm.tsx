9bc02e15776d00f35526f9bc0f547554368f2534
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-06-07 14:11:47 +0900

    fix: 파일명 수정 적용

--- src/app/(sign)/_component/RegisterForm.tsx
+++ src/app/(sign)/_component/RegisterForm.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useValidate } from '@/store/userStore';
-import ProfilForm from './_RegisterForm/ProfilForm';
+import ProfileForm from './_RegisterForm/ProfileForm';
 import UserInfoForm from './_RegisterForm/UserInfoForm';
 
 export default function RegisterForm() {
   const validate = useValidate();
-  return <>{validate.nextCheck ? <ProfilForm /> : <UserInfoForm />}</>;
+  return <>{validate.nextCheck ? <ProfileForm /> : <UserInfoForm />}</>;
 }
