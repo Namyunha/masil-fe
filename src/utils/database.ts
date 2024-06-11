af9fa7b6710e02e6890de4576c411157956a675d
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-06-11 17:40:56 +0900

    refactor: database 파일정리

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -4,30 +4,18 @@ import { MongoClient } from 'mongodb';
 const uri = process.env.DATABASE_URL;
 export const dbClient = new MongoClient(uri);
 
-export async function findUserList() {
-  try {
-    // Connect to the Atlas cluster
-    await dbClient.connect();
-    // Get the database and collection on which to run the operation
-    const db = dbClient.db('masil');
-    const col = db.collection('user');
-    const document = await col.find().toArray();
-    // Print results
-    console.log('Document found:\n' + JSON.stringify(document));
-  } catch (err) {
-    console.log(err.stack);
-  } finally {
-    await dbClient.close();
-  }
-}
+const connectDB = async () => {
+  // Connect to the Atlas cluster
+  await dbClient.connect();
+  // Get the database and collection on which to run the operation
+  const db = dbClient.db('masil');
+  const col = db.collection('user');
+  return col;
+};
 
 export async function findUser(searchEmail: string) {
   try {
-    // Connect to the Atlas cluster
-    await dbClient.connect();
-    // Get the database and collection on which to run the operation
-    const db = dbClient.db('masil');
-    const col = db.collection('user');
+    const col = await connectDB();
     const document = await col.findOne({ email: searchEmail });
     // Print results
     // console.log('Document found:\n' + JSON.stringify(document));
@@ -40,14 +28,8 @@ export async function findUser(searchEmail: string) {
 }
 
 export async function registerUser(newItem) {
-  console.log('newItem = ', newItem);
   try {
-    // Connect to the Atlas cluster
-    await dbClient.connect();
-    // Get the database and collection on which to run the operation
-    const db = dbClient.db('masil');
-    const col = db.collection('user');
-
+    const col = await connectDB();
     const document = await col
       .insertOne(newItem)
       .then((result) =>
