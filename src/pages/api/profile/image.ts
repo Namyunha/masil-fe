681c127c3b258fad817c07a284f8144e73a997ab
aaaz425 <ytokogg@gmail.com>
2024-06-13 17:26:12 +0900

    fix: 빌드 에러 해결

--- src/pages/api/profile/image.ts
+++ src/pages/api/profile/image.ts
@@ -1,6 +1,10 @@
 import aws from 'aws-sdk';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(request, response) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   aws.config.update({
     accessKeyId: process.env.IMG_ACCESS_KEY,
     secretAccessKey: process.env.IMG_SECRET_KEY,
@@ -11,12 +15,12 @@ export default async function handler(request, response) {
   const s3 = new aws.S3();
   const url = await s3.createPresignedPost({
     Bucket: process.env.AWS_BUCKET_NAME,
-    Fields: { key: request.query.file },
+    Fields: { key: req.query.file },
     Expires: 60, // seconds
     Conditions: [
       ['content-length-range', 0, 1048576], //파일용량 1MB 까지 제한
     ],
   });
 
-  response.status(200).json(url);
+  res.status(200).json(url);
 }
