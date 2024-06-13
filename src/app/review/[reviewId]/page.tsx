b06fe1b15f48681138eb5df9ffdee8226dbc3aa7
aaaz425 <ytokogg@gmail.com>
2024-06-13 17:06:41 +0900

    fix: 빌드 에러 수정
    - useParams 타입 에러 해결

--- src/app/review/[reviewId]/page.tsx
+++ src/app/review/[reviewId]/page.tsx
@@ -3,7 +3,9 @@
 import { useParams } from 'next/navigation';
 
 export default function ReviewDetail() {
-  const { reviewId } = useParams();
+  const params = useParams();
+
+  const reviewId = params && params.reviewId;
 
   return <>상세 리뷰: {reviewId}</>;
 }
