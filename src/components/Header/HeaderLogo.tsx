08db884b7cf532459e5df5dd2f880759f448f501
aaaz425 <ytokogg@gmail.com>
2024-06-16 23:58:17 +0900

    feat: 변경된 피그마 디자인 시스템 적용

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -6,7 +6,7 @@ export default function HeaderLogo() {
   return (
     <Link href={ROUTE_PATH.HOME} className="hover:cursor-pointer p-1">
       <h1>
-        <Icon name="logo" size={32} />
+        <Icon name="logo_secondary" size={32} />
       </h1>
     </Link>
   );
