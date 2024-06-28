129b763aeceb232bd3b034aa351424502d9c6b6a
aaaz425 <ytokogg@gmail.com>
2024-06-29 05:59:30 +0900

    refactor: 사이드 네비게이션 바 컴포넌트 분리 및 UI 세부 작업

---
+++ src/constants/sideNavBar.ts
@@ -0,0 +1,24 @@
+import { ROUTE_PATH } from './route';
+
+export const SIDE_NAV_LIST = [
+  {
+    routePath: ROUTE_PATH.HOME,
+    name: '리뷰',
+  },
+  {
+    routePath: ROUTE_PATH.CAFE,
+    name: '카페',
+  },
+  {
+    routePath: ROUTE_PATH.SCRAP,
+    name: '스크랩',
+  },
+  {
+    routePath: ROUTE_PATH.MY_PAGE,
+    name: '마이페이지',
+  },
+  {
+    routePath: ROUTE_PATH.WRITE,
+    name: '리뷰 작성하기',
+  },
+];
