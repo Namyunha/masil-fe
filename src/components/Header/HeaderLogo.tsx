42765b5e5c70d9f143ec0b9991fac5d1d4026115
aaaz425 <ytokogg@gmail.com>
2024-07-06 22:42:53 +0900

    refactor: 재사용을 위한 헤더 로고와 아이콘 분리

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -1,13 +1,11 @@
 import Link from 'next/link';
 import { ROUTE_PATH } from '@/constants/route';
-import Icon from '../Icon';
+import HeaderIcon from './HeaderIcon';
 
 export default function HeaderLogo() {
   return (
     <Link href={ROUTE_PATH.HOME} className="hover:cursor-pointer p-1">
-      <h1>
-        <Icon name="logo_secondary" size={32} />
-      </h1>
+      <HeaderIcon />
     </Link>
   );
 }
