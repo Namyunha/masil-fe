6f8d978cfd3e9abe749e967b6289140c6a998d50
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-07-09 10:47:31 +0900

    fix: 비회원 redirect url 변경

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const accessToken = request.cookies.get('accessToken')?.value;
   if (!accessToken && !request.nextUrl.pathname.startsWith('/login')) {
-    return Response.redirect(new URL('/register', request.url));
+    return Response.redirect(new URL('/login', request.url));
   }
 }
 // 보안처리 페이지
