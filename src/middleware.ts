f0b6a6ef19b4c89b15cab68922fd776b2642f8c2
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-07-09 19:16:24 +0900

    fix: middleware 로그인페이지 접근제한

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,21 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const accessToken = request.cookies.get('accessToken')?.value;
-  if (!accessToken && !request.nextUrl.pathname.startsWith('/login')) {
-    return Response.redirect(new URL('/login', request.url));
+
+  if (accessToken) {
+    // accessToken이 존재하는데 로그인 페이지로 접근하려는 경우
+    if (request.nextUrl.pathname.startsWith('/login')) {
+      return Response.redirect(new URL('/', request.url));
+    }
+  } else {
+    // accessToken이 없고 로그인 페이지가 아닌 다른 페이지로 접근하려는 경우
+    if (!request.nextUrl.pathname.startsWith('/login')) {
+      return Response.redirect(new URL('/login', request.url));
+    }
   }
 }
+
 // 보안처리 페이지
 export const config = {
-  matcher: ['/write'],
+  matcher: ['/write', '/login'],
 };
