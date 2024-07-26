0869957c5a1c220668d1a24c519a0ba9d1a8771b
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-07-26 14:40:11 +0900

    feat: 별점 기능 구현

---
+++ src/app/review/write/_components/RateStars.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import React, { useState } from 'react';
+import Icon from '@/components/Icon';
+import { reviewStore } from '@/store/userStore';
+
+const StarRating = () => {
+  const reviewState = reviewStore();
+  const starArray = [1, 2, 3, 4, 5];
+  const [coloredStar, setColoredStar] = useState(0);
+  const [colorFixed, setColorFixed] = useState(false);
+
+  const hoverStarHandler = (el: number) => {
+    !colorFixed && setColoredStar(el);
+  };
+
+  const checkStarHandler = (el: number) => {
+    setColorFixed(true);
+    setColoredStar(el);
+    reviewState.setRate(el);
+  };
+
+  return (
+    <div className="flex justify-center space-x-1">
+      {starArray.map((el, idx) => (
+        <span
+          className="cursor-pointer my-5"
+          onMouseEnter={() => hoverStarHandler(el)}
+          onClick={() => checkStarHandler(el)}
+          key={idx}>
+          {coloredStar >= el ? (
+            <Icon name="star" filter="PRIMARY" />
+          ) : (
+            <Icon name="star_empty" />
+          )}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default StarRating;
