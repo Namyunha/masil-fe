244a97abd3c427290b0f49299d7e7041d17b01a3
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-08-03 23:58:28 +0900

    refactor: detailImg 리팩토링

--- src/app/review/write/_components/Main.tsx
+++ src/app/review/write/_components/Main.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import clsx from 'clsx';
-import Image from 'next/image';
 import React, { useEffect, useRef, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import Icon from '@/components/Icon/Icon';
@@ -9,6 +7,7 @@ import { reviewStore } from '@/store/userStore';
 import { reviewFormInputs } from '@/types/user/form';
 import ReviewDetailRating from '../../[reviewId]/_components/ReviewDetailRating';
 import ReviewDetailTags from '../../[reviewId]/_components/ReviewDetailTags';
+import DetailReviewImg from './DetailReviewImg';
 import Footer from './Footer';
 import Header from './Header';
 import ReviewImg from './ReviewImg';
@@ -119,56 +118,14 @@ export default function Main() {
 
       {reviewStatus.modalState && <ReviewModal />}
       {imgModal && urlList && urlList?.length > 0 && (
-        <div className="absolute left-0 top-0 w-full h-full flex flex-col justify-center items-center">
-          <div
-            className="fixed z-modal left-0 right-0 top-0 mx-auto my-0 max-w-screen_max h-full bg-black opacity-80"
-            onClick={() => setImgModal((prev) => !prev)}
-          />
-          <div className="relative z-modal w-4/5 h-3/4 bg-white">
-            <span
-              onClick={() => setImgModal((prev) => !prev)}
-              className="absolute inline-block bg-button_bg_default hover:bg-button_bg_clicked p-2 rounded-full cursor-pointer top-4 right-4">
-              <Icon filter="WHITE" name="close" />
-            </span>
-            <span
-              onClick={() => onMoveIndexHandler('left')}
-              className="absolute top-1/2 cursor-pointer opacity-40 hover:opacity-100">
-              <Icon size={32} filter="WHITE" name="arrow_left" />
-            </span>
-            <span
-              onClick={() => onMoveIndexHandler('right')}
-              className="absolute top-1/2 right-0 cursor-pointer opacity-40 hover:opacity-100">
-              <Icon size={32} filter="WHITE" name="arrow_right" />
-            </span>
-            <Image
-              src={urlList[selectImgUrlIdx]}
-              alt="img"
-              width={400}
-              height={400}
-              priority
-              className="w-full h-full object-cover"
-            />
-            <div className="relative -bottom-3 ">
-              <div className="gap-2 left-0 w-full flex justify-center">
-                {urlList?.map((el, idx) => (
-                  <span
-                    onClick={() => setSelectImgUrlIdx(idx)}
-                    key={idx}
-                    className={clsx(
-                      'inline-block w-3 h-3 rounded-full cursor-pointer',
-                      selectImgUrlIdx === idx ? 'bg-primary' : 'bg-gray'
-                    )}
-                  />
-                ))}
-                <span
-                  onClick={() => onRemoveImgUrlHandler()}
-                  className="absolute cursor-pointer font-bold -bottom-2 right-2 text-gray hover:text-primary">
-                  지우기
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
+        <DetailReviewImg
+          setImgModal={setImgModal}
+          onMoveIndexHandler={onMoveIndexHandler}
+          urlList={urlList}
+          selectImgUrlIdx={selectImgUrlIdx}
+          setSelectImgUrlIdx={setSelectImgUrlIdx}
+          onRemoveImgUrlHandler={onRemoveImgUrlHandler}
+        />
       )}
       <Footer
         clickHandler={() => imgRef.current?.click()}
